bcefc52b58deac526063d0d0af1400e858373941
小星虎 <105782492+xingxinag@users.noreply.github.com>
2025-01-02 20:37:21 +0800

    更新 __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,3 @@
-"""
-MusicPlugin - 多平台点歌插件
-支持 QQ 音乐、网易云音乐、酷狗音乐的歌曲搜索功能。
-"""
-
-__version__ = "1.0.0"
-__author__ = "Your Name"
-__description__ = "支持 QQ 音乐、网易云音乐、酷狗音乐的多平台点歌插件"
 
 from .music_plugin import MusicPlugin
 
